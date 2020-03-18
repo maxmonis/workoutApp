dbe0101fdda81b2819904667f4e1737128be47f0
Max Monis <mmonis77@gmail.com>
2020-03-18 10:43:08 -0400

    refactor PreviousWorkoutApp

--- frontend/src/components/workoutComponents/PreviousWorkoutApp.js
+++ frontend/src/components/workoutComponents/PreviousWorkoutApp.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import organizeWorkout from '../../functions/organizeWorkout';
 
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 const PreviousWorkoutApp = ({ previousWorkouts }) => {
-  if (previousWorkouts.length) {
-    return (
-      <div>
-        <h2>Previous Workouts</h2>
-        {previousWorkouts.map(previousWorkout => (
-          <div key={previousWorkout.id}>
-            <h3>
-              {previousWorkout.name} - {previousWorkout.date}
-            </h3>
-            {organizeWorkout(previousWorkout.workout).map(exercise => (
-              <h3
-                key={exercise.id}
-              >{`${exercise.lift}: ${exercise.printout}`}</h3>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  }
-  return null;
+  const [displayedWorkouts, setDisplayedWorkouts] = useState(3);
+  const displayAdditionalWorkouts = () => {
+    const newDisplayedWorkouts = displayedWorkouts + 3;
+    setDisplayedWorkouts(newDisplayedWorkouts);
+  };
+  return (
+    <div style={{ alignItems: 'center' }}>
+      <Typography variant='h4'>Recent Workouts</Typography>
+      {previousWorkouts.slice(0, displayedWorkouts).map(previousWorkout => (
+        <div key={previousWorkout.id}>
+          <Typography variant='h5'>
+            {previousWorkout.name} - {previousWorkout.date}
+          </Typography>
+          {organizeWorkout(previousWorkout.workout).map(exercise => (
+            <Typography
+              variant='h6'
+              key={exercise.id}
+            >{`${exercise.lift}: ${exercise.printout}`}</Typography>
+          ))}
+        </div>
+      ))}
+      {previousWorkouts.length > displayedWorkouts && (
+        <Button onClick={displayAdditionalWorkouts}>
+          Show additional workouts...
+        </Button>
+      )}
+    </div>
+  );
 };
 
 export default PreviousWorkoutApp;
