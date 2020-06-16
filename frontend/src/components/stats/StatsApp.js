e5c031e784ea6b9a9103454cc35df9c60d3355e3
Max Monis <mmonis77@gmail.com>
2020-06-16 07:37:59 -0400

    add radios to StatsApp

--- frontend/src/components/stats/StatsApp.js
+++ frontend/src/components/stats/StatsApp.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 import Records from './Records';
 import Workouts from '../workout/Workouts';
 
 const StatsApp = ({ workouts, records }) => {
+  const [display, setDisplay] = useState('workouts');
+  const handleChange = (e) => {
+    setDisplay(e.target.value);
+  };
   return (
     <div>
-      <Workouts workouts={workouts} />
-      <Records records={records} />
+      <FormControl component='fieldset'>
+        <RadioGroup
+          row
+          aria-label='display'
+          name='display'
+          value={display}
+          onChange={handleChange}
+        >
+          <FormControlLabel
+            value='exercises'
+            control={<Radio />}
+            label='exercises'
+          />
+          <FormControlLabel
+            value='workouts'
+            control={<Radio />}
+            label='workouts'
+          />
+          <FormControlLabel
+            value='records'
+            control={<Radio />}
+            label='records'
+          />
+        </RadioGroup>
+      </FormControl>
+      {display === 'workouts' ? (
+        <Workouts workouts={workouts} />
+      ) : (
+        <Records records={records} />
+      )}
     </div>
   );
 };
