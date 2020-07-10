04d7c5ff10aa4bc9d58930c6f77e598925e4c23f
Max Monis <mmonis77@gmail.com>
2020-07-10 11:30:22 -0400

    consolidate LiftList into LiftApp

--- app/src/components/lift/LiftApp.js
+++ app/src/components/lift/LiftApp.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
 import Paper from '@material-ui/core/Paper';
+import Lift from './Lift';
 import AddLift from './AddLift';
-import LiftList from './LiftList';
 
 const LiftApp = ({ lifts, updateLifts, toggle }) => {
   return (
     <Paper className='paper narrow'>
       <AddLift updateLifts={updateLifts} />
-      <LiftList lifts={lifts} updateLifts={updateLifts} />
+      <List>
+        <TransitionGroup>
+          {lifts.map((lift) => (
+            <CSSTransition key={lift} timeout={500} classNames='fade'>
+              <Grid container justify='center' direction='row'>
+                <Grid item>
+                  <Lift lift={lift} updateLifts={updateLifts} />
+                </Grid>
+              </Grid>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      </List>
       <Button onClick={toggle} color='primary'>
         Finished Editing
       </Button>
