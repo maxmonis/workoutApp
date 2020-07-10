e7caa36109ffdcabf4bd81d3ed9782a1d454f365
Max Monis <mmonis77@gmail.com>
2020-07-10 15:44:58 -0400

    add hover event to Workout

--- app/src/components/workout/Workout.js
+++ app/src/components/workout/Workout.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Divider from '@material-ui/core/Divider';
 import EditIcon from '@material-ui/icons/Edit';
@@ -9,14 +9,24 @@ import useToggle from '../../hooks/useToggle';
 const Workout = ({
   workout,
   selected,
-  flagged,
   editingWorkout,
-  handleClick,
-  handleSelect,
-  handleDelete,
+  selectWorkout,
+  updateWorkouts,
 }) => {
   const { id, fullDate, name, routine } = workout;
+  const [isHovering, setIsHovering] = useState(false);
   const [displayMessage, toggle] = useToggle(false);
+  const handleSelect = () => {
+    if (editingWorkout && editingWorkout.id === id) {
+      selectWorkout(null);
+    } else {
+      selectWorkout(workout);
+    }
+  };
+  const handleEnter = () => setIsHovering(true);
+  const handleLeave = () => setIsHovering(false);
+  const handleToggle = () => setIsHovering(!isHovering);
+  const handleDelete = () => updateWorkouts(id);
   useEffect(() => {
     const timer = setTimeout(() => displayMessage && toggle(), 2000);
     return () => {
@@ -24,47 +34,43 @@ const Workout = ({
     };
     // eslint-disable-next-line
   }, [displayMessage]);
+  const handleClick = () => handleSelect(workout.id);
   return (
-    <div>
-      <button className='button' value={id} onClick={handleClick}>
+    <div onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
+      <h4 onClick={handleToggle}>
         {fullDate.slice(0, -3)}
         <br />
         {selected === '#' && name}
-      </button>
-      <div>
-        {flagged === id && (
-          <div>
+      </h4>
+      {isHovering && (
+        <div>
+          {!displayMessage ? (
             <div className='flex-row'>
-              <IconButton
-                color='inherit'
-                onClick={toggle}
-                onDoubleClick={handleDelete}
-              >
+              <IconButton color='inherit' onClick={toggle}>
                 <DeleteIcon aria-label='Delete' />
               </IconButton>
-              <IconButton color='inherit' onClick={handleSelect}>
+              <IconButton color='inherit' onClick={handleClick}>
                 <EditIcon aria-label='Edit' />
               </IconButton>
             </div>
-            {displayMessage && (
-              <h5>
-                Click here
-                <br />
-                <DeleteIcon
-                  style={{ cursor: 'pointer' }}
-                  onClick={handleDelete}
-                />
-                <br />
-                to delete workout
-              </h5>
-            )}
-          </div>
-        )}
-        {editingWorkout && editingWorkout.id === id && (
-          <h3 style={{ margin: '0' }}>Currently Editing</h3>
-        )}
-      </div>
-      <ul>
+          ) : (
+            <h5>
+              Click here
+              <br />
+              <DeleteIcon
+                style={{ cursor: 'pointer' }}
+                onClick={handleDelete}
+              />
+              <br />
+              to delete workout
+            </h5>
+          )}
+        </div>
+      )}
+      {editingWorkout && editingWorkout.id === id && (
+        <h3 style={{ margin: '0' }}>Currently Editing</h3>
+      )}
+      <ul onClick={handleToggle}>
         {organizeRoutine(routine).map((exercise) => (
           <li
             className='mr-40'
