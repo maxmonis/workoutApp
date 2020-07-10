35e4cfbb1798e09c6943192ca6bb416addacad15
Max Monis <mmonis77@gmail.com>
2020-07-10 11:12:07 -0400

    add copy to clipboard

--- app/src/components/layout/About.js
+++ app/src/components/layout/About.js
@@ -6,6 +6,15 @@ import useToggle from '../../hooks/useToggle';
 
 const About = () => {
   const [isDisplayed, toggle] = useToggle(false);
+  const email = 'maxwellnesscontact@gmail.com';
+  const handleClick = () => {
+    const input = document.createElement('input');
+    input.value = email;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+  };
   return (
     <div className='width-80'>
       <br />
@@ -28,11 +37,14 @@ const About = () => {
                 Access your account from anywhere in the world using a securely
                 encrypted password
                 <br /> *** <br />
-                Ad-free, free of charge and we will never share your data with
+                Free of charge, ad-free and we will never share your data with
                 any third party
                 <br /> *** <br />
                 Please direct questions, comments or concerns to
-                <br /> maxwellnesscontact@gmail.com
+                <br />
+                <span style={{ cursor: 'pointer' }} onClick={handleClick}>
+                  {email}
+                </span>
               </Typography>
               <br />
               <Button color='inherit' onClick={toggle}>
